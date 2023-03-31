c149b171ccbede535adf8df5efa46c8985938c41
wanjiru-wainaina <127139607+wanjiru-wainaina@users.noreply.github.com>
2023-03-31 12:17:50 +0300

    Introduction to PHP

---
+++ Sample-Php.php
@@ -0,0 +1,65 @@
+<?php 
+
+echo "Hello Everyone!!!!";
+
+echo"<h2> Variables in Php</h2>";
+
+echo"<p>A variable is a name memory address inside of your computer memory.</p>";
+
+echo"<p>Or you can say that a variable is a container for holding data.</p>";
+
+// Rules of Variables: 
+                    // They cannot start with a number but can contain one (number 1)
+                    // Underscores are allowed but Hyphen are'nt
+                    // Variable names are case sensitive.
+                    // ie: $age
+                    //     $Age
+                    //     $AGE
+
+
+// strings - textual data types
+
+$name = "Vivian";
+
+echo $name;
+
+echo"<hr>";
+// numerical types
+$age = 20;
+$day = 21;
+$month = "January";
+
+
+
+// contactination is joining strings and variables - we use a dot(.)
+echo "Hello everyone. my name is " .$name ."." . "<br>";
+
+echo"Iwas born i the month of " .$month .", on " .$day ."." .  "<br>";
+
+echo"That makes me " .$age . " years old."
+
+// Data Types - strings,numbers, boolians(TRUE or FALSE), arreys and objects
+
+// operators in php - arithmetiic operators, comparison operatirs and logical operators.
+// 1.Arithmetic Oparators
+    // addition +
+    // subtraction -
+    // multiplication *
+    // modulo %
+
+// 2.Comparison Operators
+    // greater than >
+    // less than <
+    // greater than or equal to >=
+    // less than or equal to <=
+    // equal to ==
+    // identical to ===
+
+// 3.Logical Operators- and, or and not
+    //  
+    
+
+
+
+
+?>
---
+++ intro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction to PHP</title>
+</head>
+<body>
+    <h3>Th syntanx for Writing PHP Scripts/Code.</h3>
+    <p>Starts with - "<?php ?>"</p>
+    <p>To display results from a php script, we use "echo"keyword</p>
+    <h4><?php echo"Greatings From Vivian"; ?></h4>
+    
+</body>
+</html>
